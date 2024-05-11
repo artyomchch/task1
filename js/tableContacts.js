618dbd6ed98db57349b6468070ddca45eb884797
artyomchch <artyomlos@yahoo.com>
2024-05-12 00:01:31 +0300

    done

--- js/tableContacts.js
+++ js/tableContacts.js
@@ -1,80 +1,26 @@
-const employe1 = {
-    firstName: 'Vladimir',
-    lastName: 'Ivanov',
-    phone: '89211231223',
-    email: 'vladimir@mail.ru',
-    companyName: 'Amazon',
-    jobTitle: 'Developer'
-}
-
-function employer (lastName, firstName, companyName) {
-    this.lastName = lastName;
-    this.firstName = firstName;
-    this.companyName = companyName
-    return lastName + " " + firstName + ", " + companyName
-}
-
-let firstEmployer = employer("Ivanov", "Vladimir", "Developer");
-let secondEmployer = employer("Sidorov", "Ivan", "Sound Tech.");
-let thirdEmployer = employer("Kozlov", "Artyom", "Journalist");
-
-console.log(firstEmployer)
-console.log(secondEmployer)
-console.log(thirdEmployer)
+const EMPLOYERS = [
+    ['Vladimir', 'Ivanov', '+7 (929) 112-32-12', 'vladimir@amazon.com', 'Amazon', 'Driver'],
+    ['Ivan', 'Sidorov', '+7 (921) 123-21-22', 'ivan@gmail.com', 'Spotify', 'Sound Tech.'],
+    ['Artyom', 'Kozlov', '+7 (926) 123-56-00', 'art@cat.it', 'Netflix', 'Mobile Dev.'],
+    ['Alexander', 'Smirnov', '+7 (999) 343-56-67', 'srt@gmail.com', 'Google', 'Data Analyst']
+]
 
 
-
-
-// let str = "Hello";
-// const str2 = "some text Hi world";
-// let str3 = "Hi"
-// console.log(str2.indexOf(str3));
-// console.log(str.includes(str2))
-// console.log(str2.toUpperCase())
-// console.log(str2.length)
-
-// let num = 1.234;
-// console.log(Math.floor(num));
-// console.log(Math.ceil(num));
-// console.log(Math.round(num));
-// console.log(+num.toFixed(2));
-
-// let str4 = '12.5px';
-
-// console.log(+str4);
-// console.log(parseInt(str4));
-// console.log(parseFloat(str4));
-
-
-// console.log(String(true));
-// console.log(String('14,5'));
-// console.log(String(undefined));
-
-
-//function getContacts(){
-//   let x = document.getElementsByTagName('td');
-//   var s = document.getElementById('employers');
-//   console.log(s)
-//   console.log(x[0])
-//   for (let curr = 1; curr < x.length; curr++){
-//     console.log(x[curr])
-//   }
-
-// let table = document.querySelector('#employers')
-// console.log(table.ariaRowIndex.length)
+document.addEventListener('DOMContentLoaded', function () {
     
-    
-   
-//}
-
-// const employe2 = employe1;
-// employe2.firstName = 'Artyom';
-// employe2.lastName = 'Kozlov';
-// employe2.phone = '89211451223';
-// employe2.email = 'art@cat.it';
-// employe2.companyName = 'Spotify';
-
-//console.log(employe1);
-//console.log(employe2);
-
-//getContacts()
+    EMPLOYERS.forEach(function(item){
+        let row = document.createElement('tr')
+        row.innerHTML = `
+        <td>${item[0]}</td>
+        <td>${item[1]}</td>
+        <td>${item[2]}</td>
+        <td>${item[3]}</td>
+        <td>${item[4]}</td>
+        <td>${item[5]}</td>
+        `
+        document.querySelector('tbody').append(row)
+    })
+  
+    document.getElementsByClassName("contacts_count")[0].innerHTML = '(' +EMPLOYERS.length + ' Contacts)'
+    document.getElementsByClassName("count_data")[0].innerHTML = 'All ' +EMPLOYERS.length
+  });
