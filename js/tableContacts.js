bcb92397ef6b2b71f0ec504d60bef3fcc993aba7
artyomchch <artyomlos@yahoo.com>
2024-05-14 15:02:23 +0300

    fix

--- js/tableContacts.js
+++ js/tableContacts.js
@@ -1,26 +1,59 @@
-const EMPLOYERS = [
-    ['Vladimir', 'Ivanov', '+7 (929) 112-32-12', 'vladimir@amazon.com', 'Amazon', 'Driver'],
-    ['Ivan', 'Sidorov', '+7 (921) 123-21-22', 'ivan@gmail.com', 'Spotify', 'Sound Tech.'],
-    ['Artyom', 'Kozlov', '+7 (926) 123-56-00', 'art@cat.it', 'Netflix', 'Mobile Dev.'],
-    ['Alexander', 'Smirnov', '+7 (999) 343-56-67', 'srt@gmail.com', 'Google', 'Data Analyst']
-]
+const employe1 = {
+    firstName: 'Vladimir',
+    lastName: 'Ivanov',
+    phone: '+7 (929) 112-32-12',
+    email: 'vladimir@amazon.com',
+    companyName: 'Amazon',
+    jobTitle: 'Driver',
+  };
+ 
+ const employe2 = {
+    firstName: 'Ivan',
+    lastName: 'Sidorov',
+    phone: '+7 (921) 123-21-22',
+    email: 'ivan@gmail.com',
+    companyName: 'Spotify',
+    jobTitle: 'Sound Tech.',
+  }; 
+  
+const employe3 = {
+    firstName: 'Artyom',
+    lastName: 'Kozlov',
+    phone: '+7 (921) 145-12-23',
+    email: 'art@cat.it',
+    companyName: 'Netflix',
+    jobTitle: 'Mobile Dev.',
+  }; 
+  
+  const employe4 = {
+    firstName: 'Alexander',
+    lastName: 'Smirnov',
+    phone: '+7 (999) 343-56-67',
+    email: 'srt@gmail.com',
+    companyName: 'Google',
+    jobTitle: 'Data Analyst',
+  };   
+
+const EMPLOYERS = [employe1, employe2, employe3, employe4]
 
 
+  
+  
 document.addEventListener('DOMContentLoaded', function () {
+    
     
     EMPLOYERS.forEach(item => {
         let row = document.createElement('tr')
-        row.innerHTML = `
-        <td>${item[0]}</td>
-        <td>${item[1]}</td>
-        <td>${item[2]}</td>
-        <td>${item[3]}</td>
-        <td>${item[4]}</td>
-        <td>${item[5]}</td>
-        `
+        row.innerHTML = Object.values(item).reduce((res, value) => {
+            return res + `<td>${value}</td>`
+        }, '')
         document.querySelector('tbody').append(row)
     })
+ 
     let counterEmployers = EMPLOYERS.length
-    document.getElementsByClassName('contacts_count')[0].innerHTML = '(' + counterEmployers + ' Contacts)'
-    document.getElementsByClassName('count_data')[0].innerHTML = 'All ' + counterEmployers
+   
+    document.querySelector('.count_data').innerHTML = 'All ' + counterEmployers
+    document.querySelector('.contacts_count').innerHTML = '(' + counterEmployers + ' Contacts)'
+    
+    
   });
